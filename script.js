9d3913a4082b1f6f335c3020fab8b90496ae5907
Ahmed Albarghouti <ahmedalbarghou@gmail.com>
2022-06-05 17:01:01 -0400

    add event listener to color picker

---
+++ script.js
@@ -0,0 +1,16 @@
+const colorPicker = document.querySelector("#colorPicker");
+let currentColor = colorPicker.value;
+
+
+
+
+
+
+colorPicker.addEventListener("change", () => {
+    currentColor = colorPicker.value;
+});
+
+
+
+
+
