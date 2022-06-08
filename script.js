97b26c16a2da7e3f35bbe99144f71ace6d9cee52
Ahmed Albarghouti <ahmedalbarghou@gmail.com>
2022-06-08 12:09:05 -0400

    add create board function that would create grid elements based on board size

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const colorPicker = document.querySelector("#colorPicker");
 const sizeSlider = document.querySelector("#sizeSlider");
 const sizeLabel = document.querySelector("#sizeLabel");
+const grid = document.querySelector("#grid");
 let currentColor = colorPicker.value;
 let currentSize = sizeSlider.value;
-
-
 
 
 
@@ -17,10 +16,22 @@ colorPicker.addEventListener("change", () => {
 sizeSlider.addEventListener("change", () => {
     currentSize = sizeSlider.value;
     sizeLabel.innerHTML = `${currentSize} x ${currentSize}`;
-   
+    createBoard();
 })
 
+function createBoard() {
+    grid.innerHTML = '';
+    grid.style.gridTemplateColumns = `repeat(${currentSize}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${currentSize}, 1fr)`;
+    
+    for (let i = 0; i<currentSize*currentSize; i++){
+        const gridElement = document.createElement("div");
+        gridElement.classList.add("grid-element");
+        grid.appendChild(gridElement);
+    }
+    
+}
 
 
-
+createBoard();
 
